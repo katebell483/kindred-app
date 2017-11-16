a8735e17fbf07cce739b7c772c6cab3a584ae202
katebell483 <kate@giphy.com>
2017-11-16 15:13:26 -0800

    wip

--- kindred-app/StudentProfileViewController.swift
+++ kindred-app/StudentProfileViewController.swift
@@ -14,16 +14,23 @@ class StudentProfileViewController: UIViewController {
     
     @IBOutlet weak var studentNameLabel: UILabel!
     @IBOutlet weak var deviceCountLabel: UILabel!
+    @IBOutlet weak var studentIconView: UIView!
     
     var studentName:String = "";
     var deviceCount:String = "";
+    
+    var deviceList = [Device]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         studentNameLabel.text = self.studentName
         deviceCountLabel.text = self.deviceCount
+        studentIconView.backgroundColor = UIColor.blue
+
         print(self.studentName)
         print(self.deviceCount)
+        
+        loadDevices()
 
         // Do any additional setup after loading the view.
     }
@@ -32,7 +39,49 @@ class StudentProfileViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    struct Device: Codable {
+        let device_uuid: String
+        let device_msg: String
+        let device_label: String
+        let device_icon: String
+    }
     
+    private func loadDevices() {
+        
+        //Implementing URLSession
+        let urlString = "http://127.0.0.1:5000/devices/" + self.studentName
+        guard let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if error != nil {
+                print(error!.localizedDescription)
+            }
+            
+            guard let data = data else { return }
+            
+            //Implement JSON decoding and parsing
+            do {
+                
+                //Decode retrived data with JSONDecoder and assing type of Article object
+                let deviceData = try JSONDecoder().decode([Device].self, from: data)
+                print(deviceData)
+                
+                //Get back to the main queue
+                DispatchQueue.main.async {
+                    self.deviceList = deviceData
+                    //self.tableView.reloadData()
+                }
+                
+            } catch let jsonError {
+                print(jsonError)
+            }
+            
+            }.resume()
+        
+        
+        //End implementing URLSession
+        
+    }
 
     /*
     // MARK: - Navigation
