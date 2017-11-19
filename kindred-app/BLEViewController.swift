fc313bb591923c6ab8846de60523e7b1c29c8c5a
katebell483 <kate@giphy.com>
2017-11-19 14:23:24 -0800

    wip

---
+++ kindred-app/BLEViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BLEViewController.swift
+//  kindred-app2
+//
+//  Created by Katherine Bell on 11/19/17.
+//  Copyright © 2017 Katherine Bell. All rights reserved.
+//
+
+import UIKit
+
+class BLEViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
