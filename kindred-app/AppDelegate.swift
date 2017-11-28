57fb9b034b9808e7694ddf45086c58d3c541add7
katebell483 <kate@giphy.com>
2017-11-27 16:14:00 -0800

    singleton. device connected

--- kindred-app/AppDelegate.swift
+++ kindred-app/AppDelegate.swift
@@ -17,7 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         // start the ble
-        BLEController().updateDevices()
+        //BLEController().updateDevices()
+        BluetoothService.shared
         
         // Override point for customization after application launch.
         return true
